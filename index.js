8ac5506c434c4564389dc70866332d8b24bb9b22
Christos <constantinides.ch@gmail.com>
2020-08-02 15:11:12 +0200

    Time for commands

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+require("dotenv").config();
 
 // our bot
 const client = new Discord.Client();
+const botToken = process.env.BOT_TOKEN;
 
-client.login(config.BOT_TOKEN);
+const prefix = "kori ";
+
+client.on("message", function (message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === "mila mou") {
+    message.channel.send("ti thelis?");
+  }
+});
+
+client.login(botToken);
